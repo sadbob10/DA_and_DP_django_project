b30b4b4995fd539a8c9e90a1a54e24c505e86efd
sadbob <abate.shallo@gmail.com>
2023-06-03 10:27:18 +0300

    First commit

---
+++ appointment/urls.py
@@ -0,0 +1,122 @@
+
+from django.urls import path
+from .import views
+from django.contrib.auth.views import LoginView,LogoutView
+
+
+urlpatterns = [
+    path('', views.home_view, name= 'home' ),
+
+
+    path('admin_click/', views.adminclick_view, name= 'admin_click' ),
+    path('doctor_click/', views.doctorclick_view, name= 'doctor_click' ),
+    path('patient_click/', views.patientclick_view, name= 'patient_click' ),
+
+
+    path('contactus/', views.contactus_view , name='contactus'),
+    path('aboutus/', views.aboutus_view , name='aboutus'),
+
+    path('adminclick', views.adminclick_view),
+    path('doctorclick', views.doctorclick_view),
+    path('patientclick', views.patientclick_view),
+
+    path('adminsignup', views.admin_signup_view),
+    path('doctorsignup', views.doctor_signup_view,name='doctorsignup'),
+    path('patientsignup', views.patient_signup_view),
+    
+    path('adminlogin', LoginView.as_view(template_name='admin/adminlogin.html')),
+    path('doctorlogin', LoginView.as_view(template_name='doctor/doctorlogin.html')),
+    path('patientlogin', LoginView.as_view(template_name='patient/patientlogin.html')),
+
+
+    path('afterlogin', views.afterlogin_view,name='afterlogin'),
+    path('logout', LogoutView.as_view(template_name='index.html'),name='logout'),
+
+
+# dashboard base view url
+
+    path('admin_base/', views.admin_base_view,name='admin_base'),
+    path('doctor_base/', views.doctor_base_view,name='admin_base'),
+    path('patient_base/', views.patient_base_view,name='admin_base'),
+
+# dashboard base view url
+
+    path('admin_card/', views.admin_card_view,name='admin_card'),
+    path('doctor_card/', views.doctor_card_view,name='doctor_card'),
+    path('patient_card/', views.patient_card_view,name='patient_card'),
+
+
+# dashboard views url
+    path('admin_dashboard/', views.admin_dashboard_view,name='admin_dashboard'),
+    path('doctor-dashboard/', views.doctor_dashboard_view,name='doctor-dashboard'),
+    path('patient-dashboard/', views.patient_dashboard_view,name='patient-dashboard'),
+
+    
+    path('admin-dashboard', views.admin_dashboard_view,name='admin-dashboard'),
+    path('admin-doctor', views.admin_doctor_view,name='admin-doctor'),
+    path('admin-view-doctor', views.admin_view_doctor_view,name='admin-view-doctor'),
+    path('delete-doctor-from-hospital/<int:pk>', views.delete_doctor_from_hospital_view,name='delete-doctor-from-hospital'),
+    path('admin-add-doctor', views.admin_add_doctor_view,name='admin-add-doctor'),
+    path('update-doctor/<int:pk>', views.update_doctor_view,name='update-doctor'),
+    path('admin-approve-doctor', views.admin_approve_doctor_view,name='admin-approve-doctor'),
+    path('approve-doctor/<int:pk>', views.approve_doctor_view,name='approve-doctor'),
+    path('reject-doctor/<int:pk>', views.reject_doctor_view,name='reject-doctor'),
+    path('admin-view-doctor-specialisation',views.admin_view_doctor_specialisation_view,name='admin-view-doctor-specialisation'),
+
+
+
+
+    path('admin-patient', views.admin_patient_view,name='admin-patient'),
+    path('admin-view-patient', views.admin_view_patient_view,name='admin-view-patient'),
+    path('delete-patient-from-hospital/<int:pk>', views.delete_patient_from_hospital_view,name='delete-patient-from-hospital'),
+    path('update-patient/<int:pk>', views.update_patient_view,name='update-patient'),
+    path('admin-add-patient', views.admin_add_patient_view,name='admin-add-patient'),
+    path('admin-approve-patient', views.admin_approve_patient_view,name='admin-approve-patient'),
+    path('approve-patient/<int:pk>', views.approve_patient_view,name='approve-patient'),
+    path('reject-patient/<int:pk>', views.reject_patient_view,name='reject-patient'),
+    path('admin-discharge-patient', views.admin_discharge_patient_view,name='admin-discharge-patient'),
+    path('discharge-patient/<int:pk>', views.discharge_patient_view,name='discharge-patient'),
+    path('download-pdf/<int:pk>', views.download_pdf_view,name='download-pdf'),
+
+
+
+
+    path('admin-appointment', views.admin_appointment_view,name='admin-appointment'),
+    path('admin-view-appointment', views.admin_view_appointment_view,name='admin-view-appointment'),
+    path('admin-add-appointment', views.admin_add_appointment_view,name='admin-add-appointment'),
+    path('admin-approve-appointment', views.admin_approve_appointment_view,name='admin-approve-appointment'),
+    path('approve-appointment/<int:pk>', views.approve_appointment_view,name='approve-appointment'),
+    path('reject-appointment/<int:pk>', views.reject_appointment_view,name='reject-appointment'),
+
+]
+
+
+#---------FOR DOCTOR RELATED URLS-------------------------------------
+urlpatterns +=[
+    path('doctor-dashboard/', views.doctor_dashboard_view,name='doctor-dashboard'),
+
+    path('doctor-patient', views.doctor_patient_view,name='doctor-patient'),
+    path('doctor-view-patient', views.doctor_view_patient_view,name='doctor-view-patient'),
+    path('doctor-view-discharge-patient',views.doctor_view_discharge_patient_view,name='doctor-view-discharge-patient'),
+
+    path('doctor-appointment', views.doctor_appointment_view,name='doctor-appointment'),
+    path('doctor-view-appointment', views.doctor_view_appointment_view,name='doctor-view-appointment'),
+    path('doctor-delete-appointment',views.doctor_delete_appointment_view,name='doctor-delete-appointment'),
+    path('delete-appointment/<int:pk>', views.delete_appointment_view,name='delete-appointment'),
+]
+
+
+
+
+#---------FOR PATIENT RELATED URLS-------------------------------------
+urlpatterns +=[
+
+    path('patient-dashboard/', views.patient_dashboard_view,name='patient-dashboard'),
+
+
+    path('patient-appointment', views.patient_appointment_view,name='patient-appointment'),
+    path('patient-book-appointment', views.patient_book_appointment_view,name='patient-book-appointment'),
+    path('patient-view-appointment', views.patient_view_appointment_view,name='patient-view-appointment'),
+    path('patient-discharge', views.patient_discharge_view,name='patient-discharge'),
+
+]
